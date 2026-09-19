a43c572441eb7f63149e5f0fde505303073dd2c3
ayanoka-bingo <dekanirlav2003@gmail.com>
2026-09-20 01:38:19 +0530

    commit two player or player VS computer

--- main.py
+++ main.py
@@ -52,10 +52,18 @@ def make_move(position :int, player: str, sets: set):
     board(position, player)
     sets.add(position)    
     DROP_LIST.remove(position)
-
+def user_move():
+    while True:
+        try:
+            position = int(input("Which position would you like to select (0-8)? "))
+            if position in DROP_LIST:
+                return position
+            print("Position already taken or invalid choice.")
+        except ValueError:
+            print("Please enter a valid number (0-8).")
 
 # Check for win------------------------------------------------------------------------------------
-def check_for_win(a:set, b:set) -> int:
+def check_for_win(a:set, b:set) -> bool | None:
     for win in WINNING_SEQUENCE:
         if win.issubset(a): 
             return True
@@ -72,7 +80,7 @@ def clear_screen():
     # Clears multi-line terminal outputs across Windows or Mac/Linux
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def board(a:tuple, player: dict):
+def board(a:int, player: str):
     # Show initial state
     clear_screen()
     print(get_table())
@@ -90,37 +98,46 @@ player_2_choice = set()
 
 print(get_table())
 def game():
-    decision = input('Wanna move first; type "y" for YES or "n" for no:\n').lower()
 
-    while decision not in ('y', 'n', 'end'):
-        decision = input('invalid input, TRY AGAIN: ').lower()
+    players = input('Play with Computer (y/n):').strip().lower()
+    while players not in ('y', 'n'):
+        players = input('invalid input, TRY AGAIN: ').strip().lower()
 
-    if decision== 'end':
-        return
-    else : 
-        user = (decision == 'y')
+    with_comp = (players== 'y')
+
+    if with_comp:
+
+        decision = input('Wanna move first; type "y" for YES or "n" for no:\n').strip().lower()
+
+        while decision not in ('y', 'n', 'end'):
+            decision = input('invalid input, TRY AGAIN: ').strip().lower()
+
+        if decision== 'end':
+            return
+        else : 
+            user = (decision == 'y')
+
+    else:
+        user = True
 
 
     while DROP_LIST:
 
         if user:            
-            while True:
-                try:
-                    position = int(input("Which position would you like to select (0-8)? "))
-                    if position in DROP_LIST:
-                        break
-                    print("Position already taken or invalid choice.")
-                except ValueError:
-                    print("Please enter a valid number (0-8).")
+            position = user_move()
             symbol = "X"
             reqd_set = player_1_choice                 
     
         else:
-            print("Computer is thinking")
-            for _ in range(3):
-                sleep(0.25)
-                print(".")
-            position = choice(DROP_LIST)
+            if with_comp:
+                print("Computer is thinking")
+                for _ in range(3):
+                    sleep(0.25)
+                    print(".")
+                position = choice(DROP_LIST)
+            else: 
+                position = user_move()
+
             symbol = 'O'
             reqd_set = player_2_choice
 
@@ -131,10 +148,11 @@ def game():
             print("Player 1 won")
             break
         elif check_for_win(player_1_choice, player_2_choice) == False:
-            print("Player 2 won")
+            print("Computer won" if with_comp else "Player 2 won")
             break
         elif len(player_2_choice) + len(player_1_choice) == len(BOARD) :
             print("IT'S A DRAW")
+            break
             
 
         user = not user
