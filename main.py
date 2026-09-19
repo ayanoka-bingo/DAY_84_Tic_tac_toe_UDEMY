9cd7d6249c6a4be5838e492bf1735c504a5e84c2
ayanoka-bingo <dekanirlav2003@gmail.com>
2026-09-20 00:16:44 +0530

    Commit One player vs Dumb computer

--- main.py
+++ main.py
@@ -5,16 +5,13 @@
 
 import os
 from time import sleep
-from random import randint
+from random import choice
 
 
 # Constants----------------------------------------------------------------------------------------
-PLAYERS = {
-    'player 1' : 'X',
-    'player 2' : 'O' ,
-}
 
-BOARD = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+BOARD = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+DROP_LIST = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
 WINNING_SEQUENCE = (
     {0, 1, 2}, {3, 4, 5}, {6, 7, 8},
@@ -50,8 +47,24 @@ def get_table():
 
 '''
 
+# Move---------------------------------------------------------------------------------------------
+def make_move(position :int, player: str, sets: set):
+    board(position, player)
+    sets.add(position)    
+    DROP_LIST.remove(position)
 
 
+# Check for win------------------------------------------------------------------------------------
+def check_for_win(a:set, b:set) -> int:
+    for win in WINNING_SEQUENCE:
+        if win.issubset(a): 
+            return True
+        if win.issubset(b):
+            return False
+    return None
+    
+
+    
 
 # Board--------------------------------------------------------------------------------------------
 
@@ -59,26 +72,73 @@ def clear_screen():
     # Clears multi-line terminal outputs across Windows or Mac/Linux
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def board():
+def board(a:tuple, player: dict):
     # Show initial state
     clear_screen()
     print(get_table())
+
+    sleep(.5)
+    BOARD[a] = player
+    clear_screen()
+    print(get_table())
+
+player_1_choice = set()
+player_2_choice = set()
+
+
+# Game---------------------------------------------------------------------------------------------
+
+print(get_table())
+def game():
+    decision = input('Wanna move first; type "y" for YES or "n" for no:\n').lower()
+
+    while decision not in ('y', 'n', 'end'):
+        decision = input('invalid input, TRY AGAIN: ').lower()
+
+    if decision== 'end':
+        return
+    else : 
+        user = (decision == 'y')
+
+
+    while DROP_LIST:
+
+        if user:            
+            while True:
+                try:
+                    position = int(input("Which position would you like to select (0-8)? "))
+                    if position in DROP_LIST:
+                        break
+                    print("Position already taken or invalid choice.")
+                except ValueError:
+                    print("Please enter a valid number (0-8).")
+            symbol = "X"
+            reqd_set = player_1_choice                 
     
-    for i in range(0, 9):
-        sleep(1)
-        BOARD[i] = 'X'
-        clear_screen()
-        print(get_table())
+        else:
+            print("Computer is thinking")
+            for _ in range(3):
+                sleep(0.25)
+                print(".")
+            position = choice(DROP_LIST)
+            symbol = 'O'
+            reqd_set = player_2_choice
 
-# Move---------------------------------------------------------------------------------------------
-def make_move():
-    pass
+        make_move(position, symbol, reqd_set)
 
 
-# Check for win------------------------------------------------------------------------------------
-def check_for_win():
-    pass
+        if check_for_win(player_1_choice, player_2_choice) == True:
+            print("Player 1 won")
+            break
+        elif check_for_win(player_1_choice, player_2_choice) == False:
+            print("Player 2 won")
+            break
+        elif len(player_2_choice) + len(player_1_choice) == len(BOARD) :
+            print("IT'S A DRAW")
+            
+
+        user = not user
 
 
 if __name__ == "__main__":
-    board()
+    game()
